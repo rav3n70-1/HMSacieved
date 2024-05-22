c946a5e331b05fec3ce23f1cc5967445751c4602
Mehedi Hasan Rohan <123560933+rav3n70-1@users.noreply.github.com>
2024-05-22 20:15:27 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='MediConnect',
+    version='2.7',
+    packages=['package'],
+    url='',
+    license='',
+    author='Rohan',
+    author_email='rohan15-5910@diu.edu.bd',
+    description='This hospital management system is designed by Rohan.',
+)
